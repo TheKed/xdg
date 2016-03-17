3ee823f5559a20321ce0a6a8b481f78cf40570d6
肖景水 <jsxiao.2009@gmail.com>
2016-03-17 20:19:01 +0800

    svc-user

---
+++ svc/svc-user-impl/src/main/java/com/ec/svc_user_impl/App.java
@@ -0,0 +1,13 @@
+package com.ec.svc_user_impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
