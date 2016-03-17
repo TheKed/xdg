70a400578890a275ecf7fb04a251e52d1e819cc7
肖景水 <jsxiao.2009@gmail.com>
2016-03-17 20:24:21 +0800

    utils

---
+++ framework/utils/src/main/java/com/ec/utils/App.java
@@ -0,0 +1,13 @@
+package com.ec.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
